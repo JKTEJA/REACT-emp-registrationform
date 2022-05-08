0c9e3eeb797914d70c24922a06eba5d9d43a53cf
JKTEJA <jakkateja94@gmail.com>
2022-05-08 14:45:07 +0530

    teja

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+// import firebaseDB from './firebase'
+const App = () => {
+  const[data,setData]=useState({
+    name:"",
+    employeeid:"",
+    phonenumber:""
+  });
+  const {name,employeeid,phonenumber}={...data}
+  const changehandler =e=>{
+    setData({...data,[e.target.name]:e.target.value});
+  }
+  const submithandler=e=>{
+    e.preventDefault();
+  
+    fetch("https://test-api-293d5-default-rtdb.firebaseio.com/data.json",
+    {
+    method:'POST',
+    body:JSON.stringify(data),
+    headers:{
+    'Content-Type':'application/json'
+    }
+  }
+    ).then(res =>alert('Data stored successfully')).catch(err =>console.log(err));
+  
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+ <div className="APP">
+   <center>
+     <h3>Employee Rigistration From</h3>
+     <form onSubmit={submithandler} style={{'border':'1px solid balck'}}>
+       <label>Name:</label><br/>
+       <input type="text" name="name" value={name} placeholder="Name" onChange={changehandler}/><br/>
+       <label>EmployeeId:</label><br/>
+       <input type="text"  name="employeeid" value={employeeid} placeholder="EmployeeId" onChange={changehandler}/><br/>
+       <label>PhoneNumber:</label><br/>
+       <input type="text" name="phonenumber" value={phonenumber} placeholder="+91" onChange={changehandler}/><br/><br></br>
+       <input className='bg bg-success text-white outlie:none' type="submit" value="login"/>
+     </form>
+   </center>
     </div>
   );
 }
